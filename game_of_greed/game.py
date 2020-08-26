7052677a2f29db0fad347408b532cf5143ec9939
mohammedghafri <eng.m.ghafri@gmail.com>
2020-08-27 00:35:17 +0300

    Bot

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,131 +1,338 @@
-from game_of_greed.game_logic import GameLogic,Banker
-# from game_logic import GameLogic,Banker
+import os
 import sys
-from collections import Counter
-#
-# from tests.flow.flo import roller
-class Game:
-    def __init__(self,roller=None):
-        self.roller = roller or GameLogic.roll_dice
-        self.shelved = 0
-        self.round = 1
-        self.total = 0
 
+# from game_of_greed.game_logic import GameLogic
+# from game_of_greed.banker import Banker
+from game_logic import GameLogic,Banker
+# from banker import Banker
+# from game_logic import GameLogic,Banker
+
+
+class Game:
+    """Class for Game of Greed application
+    """
+
+    def __init__(self, roller=None, num_rounds=20):
+
+        self._roller = roller or GameLogic.roll_dice
+        self.banker = Banker()
+        self.num_rounds = num_rounds
+        self.round_num = 0
 
     def play(self):
-        num_dice = 6
-        self.total = 0
-        print('Welcome to Game of Greed')
-        start=input('Wanna play?')
-        if start =='n':
-            print('OK. Maybe another time')
-        # x = GameLogic.roll_dice(self,6)
-        # print(x)
-        elif start == 'y':
-            flag = True
-            while flag:
-                self.shelved = 0 # to be refactored
-                print(f"Starting round {self.round}")
-                self.play_round(0)
+        """
+        Entry point for playing (or/not) a game
+        """
 
+        print("Welcome to Game of Greed")
+
+        prompt = "Wanna play?"
+
+        self.choice(prompt.strip(), self.start_game, self.decline_game)
+
+    def choice(self, prompt, accept, decline):
+
+        response = input(prompt)
+
+        if response == "y" or response == "yes":
+
+            accept()
+
+        else:
+
+            decline()
+
+    def decline_game(self):
+        print("OK. Maybe another time")
+
+    def start_game(self):
+
+        self.round_num = 1
+
+        while self.round_num <= self.num_rounds:
+
+            self.start_round(self.round_num)
+
+            self.round_num += 1
+
+            print(f"Total score is {self.banker.balance} points")
+
+        self.quit_game()
 
     def quit_game(self):
-        # print(f"Total score is {self.total} points")
-        print(f"Thanks for playing. You earned {self.total} points")
+
+        print(f"Thanks for playing. You earned {self.banker.balance} points")
+
         sys.exit()
-    def zilch(self,rounds_total):
-        print("Zilch!!! Round over")
-        rounds_total=0
-        print(f"You banked {rounds_total} points in round {self.round}")
-        print(f"Total score is {self.total} points")
-        self.round += 1
-        num_dice = 6
 
-    def cheater_or_typo(self,roll):
-        print("Cheater!!! Or possibly made a typo...")
-        print(','.join([str(i) for i in roll]))
-        more =input("Enter dice to keep (no spaces), or (q)uit: ")
+    def start_round(self, round, num_dice=6):
 
+        print(f"Starting round {round}")
 
+        round_score = 0
 
-    def play_round(self, rounds_total):
-        num_dice = 6
+        while True:
 
-        for i in range(20):
+            roll = self.roll_dice(num_dice)
 
-            print(f"Rolling {num_dice} dice...")
-
-            roll = self.roller(num_dice)
-            print(','.join([str(i) for i in roll]))
-
-            # For next version, check if ziltch here, if yes then break the round
-            # Day 3
-            cheack_before=[int(i) for i in roll]
-            check_before_start = GameLogic.calculate_score(self,cheack_before)
-            # print(check_before_start)
-            # if the input is zero its a zilch
-
-            if check_before_start == 0:
-                self.zilch(rounds_total)
+            if self.got_zilch(roll):
                 break
-            # unbanked_score=0
-            # Check what user wants to do
-            # First input trail
-            more =input("Enter dice to keep (no spaces), or (q)uit: ")
-            if more == 'q' or more =='quit':
-                print(f"Total score is {self.total} points")
+
+            keepers = self.handle_keepers(roll)
+
+            roll_again_response = input("(r)oll again, (b)ank your points or (q)uit ")
+
+            if roll_again_response == "q":
+
                 self.quit_game()
-                sys.exit()
-            more=list(more)
-            input_checkk1=[int(i) for i in tuple(more)]
-            input_checkk=Counter(input_checkk1).most_common()
 
-            roll_check=list(roll)
-            roll_check=Counter(roll_check).most_common()
-            for i in input_checkk:
-                for x in roll_check:
+                return
 
-                    if int(x[0]) == int(i[0]) and int(x[1]) >= int(i[1]):
-                        check_result=True
-                        break
-                    else:
-                        check_result=False
-            if not check_result:
-                self.cheater_or_typo(roll)
-                splitted_dice=[int(i) for i in more]
-                unbanked_score = GameLogic.calculate_score(self,(splitted_dice))
-                rounds_total += unbanked_score
-                num_dice -= len(splitted_dice)
+            elif roll_again_response == "b":
 
-            elif check_result:
-                splitted_dice=[int(i) for i in more]
-                unbanked_score = GameLogic.calculate_score(self,(splitted_dice))
-                rounds_total += unbanked_score
-                num_dice -= len(splitted_dice)
-                # print(f"You have {rounds_total} unbanked points and {num_dice} dice remaining")
-                # what_next= input("(r)oll again, (b)ank your points or (q)uit ")
-            elif more == 'q':
-                self.quit_game()
-            # else:
+                round_score = self.banker.bank()
 
-            print(f"You have {rounds_total} unbanked points and {num_dice} dice remaining")
-            # Second Input trail
-            what_next= input("(r)oll again, (b)ank your points or (q)uit ")
-
-            if what_next == 'q' or what_next == 'quit':
-                self.quit_game()
-            # if what_next == 'r' or what_next =='roll':
-
-
-            if what_next=='b' or what_next=='bank':
-                self.total += rounds_total
-                print(f"You banked {rounds_total} points in round {self.round}")
-                print(f"Total score is {self.total} points")
-                self.round += 1
-                num_dice = 6
                 break
+
+            else:
+
+                num_dice -= len(keepers)
+
+                if num_dice == 0:
+
+                    num_dice = 6
+
+        print(f"You banked {str(round_score)} points in round {round}")
+
+    def handle_keepers(self, roll):
+
+        while True:
+            keeper_string = input("Enter dice to keep (no spaces), or (q)uit: ")
+
+            if keeper_string.startswith("q"):
+                self.quit_game()
+
+            keepers = self.gather_keepers(roll, keeper_string)
+
+            roll_score = self.calculate_score(keepers)
+
+            if roll_score == 0:
+                print("Must keep at least one scoring dice")
+            else:
+                break
+
+        self.banker.shelf(roll_score)
+
+        print(
+            f"You have {self.banker.shelved} unbanked points and {len(roll) - len(keepers)} dice remaining"
+        )
+
+        return keepers
+
+    def roll_dice(self, num):
+
+        print(f"Rolling {num} dice...")
+
+        roll = self._roller(num)
+
+        print(",".join([str(i) for i in roll]))
+
+        return roll
+
+    def got_zilch(self, roll):
+
+        initial_score = self.calculate_score(roll)
+
+        if initial_score == 0:
+
+            print("Zilch!!! Round over")
+
+            self.banker.clear_shelf()
+
+            return True
+
+        return False
+
+    def calculate_score(self, roll):
+        return GameLogic.calculate_score(roll)
+
+    def keep_scorers(self, roll):
+        return GameLogic.get_scorers(roll)
+
+    def gather_keepers(self, roll, keeper_string):
+
+        keepers = [int(ch) for ch in keeper_string]
+
+        while not GameLogic.validate_keepers(roll, keepers):
+            print("Cheater!!! Or possibly made a typo...")
+            print(",".join([str(i) for i in roll]))
+            keeper_string = input("Enter dice to keep (no spaces), or (q)uit: ")
+            if keeper_string.startswith("q"):
+                self.quit_game()
+
+            keepers = [int(ch) for ch in keeper_string]
+
+        return keepers
+
+
+def clear():
+    # stretch goal to allow user to clear terminal mid game
+
+    # os.system("cls" if os.name == "nt" else "clear")
+    pass
 
 
 if __name__ == "__main__":
     game = Game()
     game.play()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from game_of_greed.game_logic import GameLogic,Banker
+# # from game_logic import GameLogic,Banker
+# import sys
+# from collections import Counter
+# #
+# # from tests.flow.flo import roller
+# class Game:
+#     def __init__(self,roller=None):
+#         self.roller = roller or GameLogic.roll_dice
+#         self.shelved = 0
+#         self.round = 1
+#         self.total = 0
+
+
+#     def play(self):
+#         num_dice = 6
+#         self.total = 0
+#         print('Welcome to Game of Greed')
+#         start=input('Wanna play?')
+#         if start =='n':
+#             print('OK. Maybe another time')
+#         # x = GameLogic.roll_dice(self,6)
+#         # print(x)
+#         elif start == 'y':
+#             flag = True
+#             while flag:
+#                 self.shelved = 0 # to be refactored
+#                 print(f"Starting round {self.round}")
+#                 self.play_round(0)
+
+
+#     def quit_game(self):
+#         # print(f"Total score is {self.total} points")
+#         print(f"Thanks for playing. You earned {self.total} points")
+#         sys.exit()
+#     def zilch(self,rounds_total):
+#         print("Zilch!!! Round over")
+#         rounds_total=0
+#         print(f"You banked {rounds_total} points in round {self.round}")
+#         print(f"Total score is {self.total} points")
+#         self.round += 1
+#         num_dice = 6
+
+#     def cheater_or_typo(self,roll):
+#         print("Cheater!!! Or possibly made a typo...")
+#         print(','.join([str(i) for i in roll]))
+#         more =input("Enter dice to keep (no spaces), or (q)uit: ")
+
+
+
+#     def play_round(self, rounds_total):
+#         num_dice = 6
+
+#         for i in range(20):
+
+#             print(f"Rolling {num_dice} dice...")
+
+#             roll = self.roller(num_dice)
+#             print(','.join([str(i) for i in roll]))
+
+#             # For next version, check if ziltch here, if yes then break the round
+#             # Day 3
+#             cheack_before=[int(i) for i in roll]
+#             check_before_start = GameLogic.calculate_score(self,cheack_before)
+#             # print(check_before_start)
+#             # if the input is zero its a zilch
+
+#             if check_before_start == 0:
+#                 self.zilch(rounds_total)
+#                 break
+#             # unbanked_score=0
+#             # Check what user wants to do
+#             # First input trail
+#             more =input("Enter dice to keep (no spaces), or (q)uit: ")
+#             if more == 'q' or more =='quit':
+#                 print(f"Total score is {self.total} points")
+#                 self.quit_game()
+#                 sys.exit()
+#             more=list(more)
+#             input_checkk1=[int(i) for i in tuple(more)]
+#             input_checkk=Counter(input_checkk1).most_common()
+
+#             roll_check=list(roll)
+#             roll_check=Counter(roll_check).most_common()
+#             for i in input_checkk:
+#                 for x in roll_check:
+
+#                     if int(x[0]) == int(i[0]) and int(x[1]) >= int(i[1]):
+#                         check_result=True
+#                         break
+#                     else:
+#                         check_result=False
+#             if not check_result:
+#                 self.cheater_or_typo(roll)
+#                 splitted_dice=[int(i) for i in more]
+#                 unbanked_score = GameLogic.calculate_score(self,(splitted_dice))
+#                 rounds_total += unbanked_score
+#                 num_dice -= len(splitted_dice)
+
+#             elif check_result:
+#                 splitted_dice=[int(i) for i in more]
+#                 unbanked_score = GameLogic.calculate_score(self,(splitted_dice))
+#                 rounds_total += unbanked_score
+#                 num_dice -= len(splitted_dice)
+#                 # print(f"You have {rounds_total} unbanked points and {num_dice} dice remaining")
+#                 # what_next= input("(r)oll again, (b)ank your points or (q)uit ")
+#             elif more == 'q':
+#                 self.quit_game()
+#             # else:
+
+#             print(f"You have {rounds_total} unbanked points and {num_dice} dice remaining")
+#             # Second Input trail
+#             what_next= input("(r)oll again, (b)ank your points or (q)uit ")
+
+#             if what_next == 'q' or what_next == 'quit':
+#                 self.quit_game()
+#             # if what_next == 'r' or what_next =='roll':
+
+
+#             if what_next=='b' or what_next=='bank':
+#                 self.total += rounds_total
+#                 print(f"You banked {rounds_total} points in round {self.round}")
+#                 print(f"Total score is {self.total} points")
+#                 self.round += 1
+#                 num_dice = 6
+#                 break
+
+
+# if __name__ == "__main__":
+#     game = Game()
+#     game.play()
--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,70 +1,172 @@
 from collections import Counter
-import random
-# from abc import staticmethod
+from random import randint
 
-class GameLogic():
-    """
-    This class is to define the logic of the game of greed for rolling random dice and counting score
 
-    """
-    def __init__(self):
-        pass
+class GameLogic:
+    @staticmethod
+    def roll_dice(num=6):
+        return tuple([randint(1, 6) for _ in range(num)])
 
-    def calculate_score(self, dice):
+    @staticmethod
+    def calculate_score(dice):
+        """
+        dice is a tuple of integers represent the user's selected dice pulled out from current roll
+        """
+
+        if len(dice) > 6:
+            raise Exception("Cheating Cheater!")
+
+        counts = Counter(dice)
+
+        if len(counts) == 6:
+            return 1500
+
+        if len(counts) == 3 and all(val == 2 for val in counts.values()):
+            return 1500
+
         score = 0
-        pairs = 0
-        coun = Counter(dice)
 
-        if coun[5] == 4 and coun[1] == 2:
-            score += 2000
-            return score
+        ones_used = fives_used = False
 
-        if len(coun) == 6:
-            score += 1500
-            return score
+        for num in range(1, 6 + 1):
 
-        for i in coun:
-            if coun[i] == 2:
-                pairs += 1
-                if pairs == 3:
-                    score = 0
-                    score += 1500
-                    return score
+            pip_count = counts[num]
 
+            if pip_count >= 3:
 
-            if coun[i] < 3 and i == 5:
-                score += (coun[i] * 50)
+                if num == 1:
 
+                    ones_used = True
 
-            if coun[i] < 3 and i == 1:
-                score += (coun[i] * 100)
+                elif num == 5:
 
+                    fives_used = True
 
-            if coun[i] >= 3 and i == 1:
-                score += 1000
-                coun[i] -= 3
-                for i in range(coun[i]):
-                    score += 1000
-                continue
+                score += num * 100
 
+                # handle 4,5,6 of a kind
+                score += score * (pip_count - 3)
 
-            if coun[i] >= 3 and i != 1:
-                m = i
-                score += (i * 100)
-                left = coun[i] - 3
-                while left > 0:
-                    score += (m * 100)
-                    left -=1
-                continue
+                # 1s are worth 10x
+                if num == 1:
+                    score *= 10
+
+        if not ones_used:
+            score += counts.get(1, 0) * 100
+
+        if not fives_used:
+            score += counts.get(5, 0) * 50
 
         return score
 
     @staticmethod
-    def roll_dice(n):
-        list = []
-        for i in range(n):
-            list.append(random.randint(1, n))
-        return(tuple(list))
+    def validate_keepers(roll, keepers):
+        return not Counter(keepers) - Counter(roll)
+
+    @staticmethod
+    def get_scorers(dice):
+        all_dice_score = GameLogic.calculate_score(dice)
+
+        if all_dice_score == 0:
+            return tuple()
+
+        scorers = []
+
+        for i in range(len(dice)):
+            sub_roll = dice[:i] + dice[i + 1 :]
+            sub_score = GameLogic.calculate_score(sub_roll)
+
+            if sub_score != all_dice_score:
+                scorers.append(dice[i])
+
+        return tuple(scorers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import Counter
+# import random
+# # from abc import staticmethod
+
+# class GameLogic():
+#     """
+#     This class is to define the logic of the game of greed for rolling random dice and counting score
+
+#     """
+#     def __init__(self):
+#         pass
+
+#     def calculate_score(self, dice):
+#         score = 0
+#         pairs = 0
+#         coun = Counter(dice)
+
+#         if coun[5] == 4 and coun[1] == 2:
+#             score += 2000
+#             return score
+
+#         if len(coun) == 6:
+#             score += 1500
+#             return score
+
+#         for i in coun:
+#             if coun[i] == 2:
+#                 pairs += 1
+#                 if pairs == 3:
+#                     score = 0
+#                     score += 1500
+#                     return score
+
+
+#             if coun[i] < 3 and i == 5:
+#                 score += (coun[i] * 50)
+
+
+#             if coun[i] < 3 and i == 1:
+#                 score += (coun[i] * 100)
+
+
+#             if coun[i] >= 3 and i == 1:
+#                 score += 1000
+#                 coun[i] -= 3
+#                 for i in range(coun[i]):
+#                     score += 1000
+#                 continue
+
+
+#             if coun[i] >= 3 and i != 1:
+#                 m = i
+#                 score += (i * 100)
+#                 left = coun[i] - 3
+#                 while left > 0:
+#                     score += (m * 100)
+#                     left -=1
+#                 continue
+
+#         return score
+
+#     @staticmethod
+#     def roll_dice(n):
+#         list = []
+#         for i in range(n):
+#             list.append(random.randint(1, n))
+#         return(tuple(list))
 
 
 class Banker():
@@ -79,17 +181,17 @@ class Banker():
         return self.shelved
     def bank(self):
         self.balance += self.shelved
-        self.clear_shlef()
+        self.clear_shelf()
         return self.balance
-    def clear_shlef(self):
+    def clear_shelf(self):
         self.shelved = 0
         return self.shelved
 
 
-if __name__ == "__main__":
-    n = GameLogic()
-    print(n.roll_dice(6))
-    # print(n.calculate_score())
-    # print(GameLogic.calculate_score((5,)))
-    y = GameLogic()
-    print(y.calculate_score((5,)))
+# if __name__ == "__main__":
+#     n = GameLogic()
+#     print(n.roll_dice(6))
+#     # print(n.calculate_score())
+#     # print(GameLogic.calculate_score((5,)))
+#     y = GameLogic()
+#     print(y.calculate_score((5,)))
